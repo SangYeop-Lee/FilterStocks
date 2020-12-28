913b6e93bcaa473bad4acd12da82c35f861a4eb7
sangyeop-lee <ryan080198@gmail.com>
2020-12-28 14:55:05 +0000

    add KODEX200 corporations data

--- model/stockPrice.js
+++ model/stockPrice.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const stockPriceSchema = new mongoose.Schema({
-	name: String,
+	corpName: String,
 	stockCode: String,
-	data: [{date: String, price: Number}]
+	data: [{date: Date, price: Number}]
 });
-
-
 
 // https://mongoosejs.com/docs/subdocs.html
 // https://mongoosejs.com/docs/api.html#mongoosearray_MongooseArray-addToSet
